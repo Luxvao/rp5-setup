aa9e75f66707745391e0b1012740c4c4496a17a4
Bor Grofelnik <luxvoo.me@gmail.com>
2024-12-19 20:13:53 +0100

    Temporary fan solution

---
+++ fan_tmp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Create a systemd service that silences the fan
+cat <<EOF >> /etc/systemd/system/fan.service
+[Unit]
+Description=Silences the RP5 fan
+DefaultDependencies=no
+Before=basic.target
+After=local-fs.target
+
+[Service]
+Type=simple
+ExecStart=echo 30 > /sys/class/hwmon/hwmon0/pwm1
+Restart=no
+User=root
+Group=root
+
+[Install]
+WantedBy=basic.target
+EOF
+
+# Reload daemon
+systemctl daemon-reload
+
+# Enable & start the service
+systemctl enable --now fan
